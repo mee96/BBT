99815553388bced79caee684f1ed2d45c23b4fa4
Carmee <carmenmc96@gmail.com>
2026-05-22 19:03:30 +0200

    footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
